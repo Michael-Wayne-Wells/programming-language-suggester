59df5c77cd516ed838382b669d5a59259b84385d
Michael Wells <mwells1286@gmail.com>
2019-10-25 10:31:43 -0700

    add javascript that takes user input and outputs a suggested programming language.

---
+++ js/scripts.js
@@ -0,0 +1,24 @@
+$(document).ready(function() {
+  $("button.submit").click(function(event){
+    $(".survey").hide();
+    event.preventDefault();
+      var creativity  = parseInt($('input:radio[name=creativity]:checked').val());
+      var fear = parseInt($('input:radio[name=fear]:checked').val());
+      var business = parseInt($('input:radio[name=business]:checked').val());
+      var workTraits = creativity + business;
+      console.log(workTraits, business, fear, creativity);
+      if (workTraits <= 3 && fear === 1) {
+        $('#ruby').show();
+      } else if (workTraits > 3 && fear >= 2) {
+        $('#csharp').show();
+      } else if (workTraits > 3 && fear === 1) {
+        $('#python').show();
+      } else {
+        $('#singularity').show();
+        document.body.style.backgroundColor = "black";
+      }
+    });
+    $("button.restart").click(function(event){
+    location.reload();
+    });
+  });
