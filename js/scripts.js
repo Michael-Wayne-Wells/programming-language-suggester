cd9bbef083df319e9bf08a2b443f8e5d115e7545
Michael Wells <mwells1286@gmail.com>
2019-10-25 16:49:29 -0700

    clean up on index.html, styles.css, and scripts.jss.

--- js/scripts.js
+++ js/scripts.js
@@ -1,42 +1,42 @@
 $(document).ready(function() {
   $("button.submit").click(function(event){
     $(".survey").hide();
-      event.preventDefault();
-      var firstName = $('#first-name').val();
-      var lastName = $('#last-name').val();
-      var creativity = parseInt($('input:radio[name=creativity]:checked').val());
-      var fear = parseInt($('input:radio[name=fear]:checked').val());
-      var business = parseInt($('input:radio[name=business]:checked').val());
-      var workTraits = creativity + business;
-      var userName = firstName + lastName;
-      var favNumber = $('#fav-number').val();
-      var favFood = $('#fav-food').val();
-      $('.user-name').text(firstName);
-      $('.food').text(favFood);
-      if (userName === "JohnConnor") {
-        $('#victory').show();
-        document.body.style.backgroundColor = "black";
-      } else if (favNumber === "01101000 01101001") {
-        $('#robot').show();
-      } else if (business === 3 && creativity === 3 && fear === 1) {
-        $('#cats').show();
-      } else if (fear === 3 || creativity === 2 && fear >= 3){
-        $('#singularity').show();
-        document.body.style.backgroundColor = "black";
-      } else if(workTraits <= 3 && fear === 4) {
-        $('#ruby').show();
-      } else if (workTraits < 3 && fear <= 2) {
-        $('#csharp').show();
-      } else if (workTraits >= 3 && fear === 1) {
-        $('#python').show();
-      } else {
-        $('#javascript').show();
-      }
-    });
-    $("button.restart").click(function(event) {
+    event.preventDefault();
+    var firstName = $('#first-name').val();
+    var lastName = $('#last-name').val();
+    var creativity = parseInt($('input:radio[name=creativity]:checked').val());
+    var fear = parseInt($('input:radio[name=fear]:checked').val());
+    var business = parseInt($('input:radio[name=business]:checked').val());
+    var workTraits = creativity + business;
+    var userName = firstName + lastName;
+    var favNumber = $('#fav-number').val();
+    var favFood = $('#fav-food').val();
+    $('.user-name').text(firstName);
+    $('.food').text(favFood);
+    if (userName === "JohnConnor") {
+      $('#victory').show();
+      document.body.style.backgroundColor = "black";
+    } else if (favNumber === "01101000 01101001") {
+      $('#robot').show();
+    } else if (business === 3 && creativity === 3 && fear === 1) {
+      $('#cats').show();
+    } else if (fear === 3 || creativity === 2 && fear >= 3){
+      $('#singularity').show();
+      document.body.style.backgroundColor = "black";
+    } else if(workTraits <= 3 && fear === 4) {
+      $('#ruby').show();
+    } else if (workTraits < 3 && fear <= 2) {
+      $('#csharp').show();
+    } else if (workTraits >= 3 && fear === 1) {
+      $('#python').show();
+    } else {
+      $('#javascript').show();
+    }
+  });
+  $("button.restart").click(function(event) {
     location.reload();
-    });
-    $("#click-to-hide").click(function() {
+  });
+  $("#click-to-hide").click(function() {
     $(this).remove();
     $('#singularity').hide();
     $('#victory').show();
