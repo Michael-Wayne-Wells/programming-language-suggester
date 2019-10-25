86443708a0a21a713a20dd8181f1552a5e997e7f
Michael Wells <mwells1286@gmail.com>
2019-10-25 10:41:14 -0700

    add variables in javascript that will take the users first and last name.

--- js/scripts.js
+++ js/scripts.js
@@ -2,11 +2,13 @@ $(document).ready(function() {
   $("button.submit").click(function(event){
     $(".survey").hide();
     event.preventDefault();
-      var creativity  = parseInt($('input:radio[name=creativity]:checked').val());
+      var firstName = $('#first-name').val();
+      // var lastName =
+      var creativity = parseInt($('input:radio[name=creativity]:checked').val());
       var fear = parseInt($('input:radio[name=fear]:checked').val());
       var business = parseInt($('input:radio[name=business]:checked').val());
       var workTraits = creativity + business;
-      console.log(workTraits, business, fear, creativity);
+      console.log(workTraits, business, fear, creativity, firstName);
       if (workTraits <= 3 && fear === 1) {
         $('#ruby').show();
       } else if (workTraits > 3 && fear >= 2) {
